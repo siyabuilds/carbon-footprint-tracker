f94a7d532509b059b816a653ffed77334a115a17
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2025-08-09 14:24:58 +0200

    feat: implement MongoDB connection in connect.js and integrate it in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "../db/connect.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("App is running!");
