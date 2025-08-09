27ba2cfbf4e91089b0c94b497f7ffc9078dffb04
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2025-08-09 14:46:34 +0200

    feat: set up user registration route and enable JSON parsing in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../db/connect.js";
+import router from "./routes/register.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/register", router);
 
 connectDB();
 
